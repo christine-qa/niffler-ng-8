9ba8dd8918d4b6fd9383d9fc10810b58c5dd1755
Ананченко Кристина Николаевна <kananchenko@ecom.tech>
2025-04-08 00:01:43 +0300

    profile test from extension lesson 2

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTests.java
@@ -0,0 +1,33 @@
+package guru.qa.niffler.test.web;
+
+import guru.qa.niffler.jupiter.extension.UsersQueueExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(UsersQueueExtension.class)
+public class ProfileTests {
+
+    @Test
+    void testWithEmptyUser0(@UsersQueueExtension.UserType(empty = true) UsersQueueExtension.StaticUser user) throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println(user);
+    }
+
+    @Test
+    void testWithEmptyUser1(@UsersQueueExtension.UserType(empty = true) UsersQueueExtension.StaticUser user) throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println(user);
+    }
+
+    @Test
+    void testWithNotEmptyUser0(@UsersQueueExtension.UserType(empty = false) UsersQueueExtension.StaticUser user) throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println(user);
+    }
+
+    @Test
+    void testWithNotEmptyUser1(@UsersQueueExtension.UserType(empty = false) UsersQueueExtension.StaticUser user) throws InterruptedException {
+        Thread.sleep(1000);
+
+    }
+}
