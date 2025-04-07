fef863fff999719d3138df2a39bfd863b777afaf
Ананченко Кристина Николаевна <kananchenko@ecom.tech>
2025-04-07 23:45:16 +0300

    hw1 and extension lesson2 without hw

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/DisabledByIssue.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/DisabledByIssue.java
@@ -1,17 +1,16 @@
-package guru.qa.niffler.jupiter;
+package guru.qa.niffler.jupiter.annotation;
 
+import guru.qa.niffler.jupiter.extension.IssueExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-@ExtendWith(CategoryExtension.class)
-public @interface Category {
-
-    String username();
-
-    boolean archived();
+@ExtendWith(IssueExtension.class)
+public @interface DisabledByIssue {
+  String value();
 }
